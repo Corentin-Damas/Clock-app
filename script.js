7340f3b6fe4d72db06555cdb189f0eb1d3429993
Nazemrap <corentin.damas@gmail.com>
2023-06-24 11:31:41 +0200

    general layout - btn to reshape

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+// Get random programming quote  at the start and on clic
+// View the current time and location information based on their IP address
+// Layout change depending on the day period (night/ day)
+// Show Current Timezone / Day of the year / day of the week / week number
+
