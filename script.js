0628726c79ee25c633d43bb226a6b0fcc6b7d730
Nazemrap <corentin.damas@gmail.com>
2023-06-24 12:54:55 +0200

    general design fix - start script get quote

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 
 
-// Get random programming quote  at the start and on clic
 // View the current time and location information based on their IP address
 // Layout change depending on the day period (night/ day)
 // Show Current Timezone / Day of the year / day of the week / week number
 
+
+// Get random programming quote  at the start and on clic
+
+URL = "https://api.chucknorris.io/jokes/random"
+
+async function logJSONData() {
+    const response = await fetch(URL);
+    const jsonData = await response.json();
+    console.log(jsonData);
+  }
